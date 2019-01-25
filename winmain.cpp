191a882635ade64f74e282ad05b620b1ccc4ac0e
alexg <alex@urghh.net>
2019-01-25 18:51:01 +0200

    init

---
+++ winmain.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+int WinMain(HINSTANCE hInstance,
+            HINSTANCE hPrevInstance, 
+            LPTSTR    lpCmdLine, 
+            int       cmdShow)
+    {
+    /* do your stuff here. If you return from this function the program ends */
+    }
